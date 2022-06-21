4b74daea368d37f4ccd68838f68cd2c1db8381e6
Amar Singh Yadav <amarprince9@gmail.comm>
2022-06-21 12:43:57 +0530

    Responsive Table Demo

--- src/App.js
+++ src/App.js
@@ -31,9 +31,148 @@ const App = () => {
   };
 
   return (
+    // <div>
+    //   <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
+    //   <Expenses item={expenses} />
+    // </div>
     <div>
-      <NewExpense onAddExpense={addExpenseHandler}></NewExpense>
-      <Expenses item={expenses} />
+      <div className="table-container">
+        <h1 className="heading">Resposive Table</h1>
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Start Date</th>
+              <th>Start / End Time</th>
+              <th>Batch Type</th>
+              <th>Training Mode</th>
+              <th>Batch Staus</th>
+              <th>#</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td data-lable="Start Date"> 1 Jan 2021</td>
+              <td data-lable="Start / End Time"> 09:00-13:00 IST</td>
+              <td data-lable="Batch Type"> Weekend</td>
+              <td data-lable="Training Mode"> Online Traning</td>
+              <td data-lable="Batch Staus"> Open</td>
+              <td data-lable="#">
+                <a href="#" className="btn">
+                  Button-1
+                </a>
+                <a href="#" className="btn">
+                  Button-2
+                </a>
+                <a href="#" className="btn">
+                  Button-3
+                </a>
+                <a href="#" className="btn">
+                  Button-4
+                </a>
+                <a href="#" className="btn">
+                  Button-5
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td data-lable="Start Date"> 1 Jan 2021</td>
+              <td data-lable="Start / End Time"> 09:00-13:00 IST</td>
+              <td data-lable="Batch Type"> Weekend</td>
+              <td data-lable="Training Mode"> Online Traning</td>
+              <td data-lable="Batch Staus"> Open</td>
+              <td data-lable="#">
+                <a href="#" className="btn">
+                  Button-1
+                </a>
+                <a href="#" className="btn">
+                  Button-2
+                </a>
+                <a href="#" className="btn">
+                  Button-3
+                </a>
+                <a href="#" className="btn">
+                  Button-4
+                </a>
+                <a href="#" className="btn">
+                  Button-5
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td data-lable="Start Date"> 1 Jan 2021</td>
+              <td data-lable="Start / End Time"> 09:00-13:00 IST</td>
+              <td data-lable="Batch Type"> Weekend</td>
+              <td data-lable="Training Mode"> Online Traning</td>
+              <td data-lable="Batch Staus"> Open</td>
+              <td data-lable="#">
+                <a href="#" className="btn">
+                  Button-1
+                </a>
+                <a href="#" className="btn">
+                  Button-2
+                </a>
+                <a href="#" className="btn">
+                  Button-3
+                </a>
+                <a href="#" className="btn">
+                  Button-4
+                </a>
+                <a href="#" className="btn">
+                  Button-5
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td data-lable="Start Date"> 1 Jan 2021</td>
+              <td data-lable="Start / End Time"> 09:00-13:00 IST</td>
+              <td data-lable="Batch Type"> Weekend</td>
+              <td data-lable="Training Mode"> Online Traning</td>
+              <td data-lable="Batch Staus"> Open</td>
+              <td data-lable="#">
+                <a href="#" className="btn">
+                  Button-1
+                </a>
+                <a href="#" className="btn">
+                  Button-2
+                </a>
+                <a href="#" className="btn">
+                  Button-3
+                </a>
+                <a href="#" className="btn">
+                  Button-4
+                </a>
+                <a href="#" className="btn">
+                  Button-5
+                </a>
+              </td>
+            </tr>
+            <tr>
+              <td data-lable="Start Date"> 1 Jan 2021</td>
+              <td data-lable="Start / End Time"> 09:00-13:00 IST</td>
+              <td data-lable="Batch Type"> Weekend</td>
+              <td data-lable="Training Mode"> Online Traning</td>
+              <td data-lable="Batch Staus"> Open</td>
+              <td data-lable="#">
+                <a href="#" className="btn">
+                  Button-1
+                </a>
+                <a href="#" className="btn">
+                  Button-2
+                </a>
+                <a href="#" className="btn">
+                  Button-3
+                </a>
+                <a href="#" className="btn">
+                  Button-4
+                </a>
+                <a href="#" className="btn">
+                  Button-5
+                </a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
